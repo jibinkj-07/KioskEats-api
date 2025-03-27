1c142c11571b749c9f84ff4f1302d885fa9d3aca
jibinkj-07 <jibinkj009@hotmail.com>
2025-03-27 16:56:06 +0000

    Added store GET endpoints

--- core/routes/store.route.js
+++ core/routes/store.route.js
@@ -1,10 +1,21 @@
 const router = require("express").Router();
 const verifyjwt = require("../../lib/middleware/verifyjwt");
+const isAdmin = require("../../lib/middleware/isAdmin");
 const controller = require("../../lib/controllers/store.controller");
 
 // GET METHODS
+router.get("/", controller.getAllStores); // All Stores details
+router.get("/owner/:ownerId", controller.getStoreByOwner); // All Stores by ownerId
+
+router.get("/:storeId", controller.getStore); // Store basic details
+router.get("/:storeId/status", controller.getStoreStatus); // Store status
+router.get("/:storeId/menu", controller.getStoreMenu); // Store menuItems
+router.get("/:storeId/categories", controller.getStoreCategories); // Store categories
+router.get("/:storeId/orders", verifyjwt, controller.getStoreOrders); // Store orders
+router.get("/:storeId/offers", controller.getStoreOffers); // Store offer
+router.get("/:storeId/feedback", controller.getStoreFeedback); // Store feedback
 
 // POST METHODS
-router.post("/", verifyjwt, controller.createStore);
+router.post("/", verifyjwt, isAdmin, controller.createStore);
 
 module.exports = router;
