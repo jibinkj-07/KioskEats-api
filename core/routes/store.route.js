363cf1a0191b3f19b90d2b3e85eb39a04c9de30f
jibinkj-07 <jibinkj009@hotmail.com>
2025-03-28 14:40:17 +0000

    Added menu item controller and logics

--- core/routes/store.route.js
+++ core/routes/store.route.js
@@ -1,26 +1,52 @@
 const router = require("express").Router();
 const verifyjwt = require("../../lib/middleware/verifyjwt");
 const isAdmin = require("../../lib/middleware/isAdmin");
-const controller = require("../../lib/controllers/store.controller");
+const storeController = require("../../lib/controllers/store.controller");
+const menuController = require("../../lib/controllers/menu.controller");
 
 // GET METHODS
-router.get("/", controller.getAllStores); // All Stores details
-router.get("/owner/:ownerId", controller.getStoreByOwner); // All Stores by ownerId
+router.get("/", storeController.getAllStores); // All Stores details
+router.get("/owner/:ownerId", storeController.getStoreByOwner); // All Stores by ownerId
 
-router.get("/:storeId", controller.getStore); // Store basic details
-router.get("/:storeId/status", controller.getStoreStatus); // Store status
-router.get("/:storeId/menu", controller.getStoreMenu); // Store menuItems
-router.get("/:storeId/categories", controller.getStoreCategories); // Store categories
-router.get("/:storeId/orders", verifyjwt, controller.getStoreOrders); // Store orders
-router.get("/:storeId/offers", controller.getStoreOffers); // Store offer
-router.get("/:storeId/feedback", controller.getStoreFeedback); // Store feedback
+router.get("/:storeId", storeController.getStore); // Store basic details
+router.get("/:storeId/status", storeController.getStoreStatus); // Store status
+router.get("/:storeId/menu", menuController.getMenuItems); // Store menuItems
+router.get("/:storeId/categories", storeController.getStoreCategories); // Store categories
+router.get("/:storeId/orders", verifyjwt, storeController.getStoreOrders); // Store orders
+router.get("/:storeId/offers", storeController.getStoreOffers); // Store offer
+router.get("/:storeId/feedback", storeController.getStoreFeedback); // Store feedback
 
 // POST METHODS
-router.post("/", verifyjwt, isAdmin, controller.createStore);
+router.post("/", verifyjwt, isAdmin, storeController.createStore);
+router.post("/:storeId/menu", verifyjwt, isAdmin, menuController.addMenuItem);
 
 // PATCH METHODS
-router.patch("/:storeId/details", verifyjwt, isAdmin, controller.updateStore);
+router.patch(
+  "/:storeId/details",
+  verifyjwt,
+  isAdmin,
+  storeController.updateStore
+);
+router.patch(
+  "/:storeId/menu/:itemId",
+  verifyjwt,
+  isAdmin,
+  menuController.updateMenuItem
+);
 
 // DELETE METHODS
-router.delete("/:storeId", verifyjwt, isAdmin, controller.deleteStore);
+router.delete("/:storeId", verifyjwt, isAdmin, storeController.deleteStore);
+router.delete(
+  "/:storeId/menu/:itemId",
+  verifyjwt,
+  isAdmin,
+  menuController.deleteMenuItem
+);
+router.delete(
+  "/:storeId/menu",
+  verifyjwt,
+  isAdmin,
+  menuController.deleteAllMenu
+);
+
 module.exports = router;
