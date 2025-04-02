6b4238921c6c5a5bfcdac107f64f0ce5f6eccbe3
jibinkj-07 <jibinkj009@hotmail.com>
2025-04-02 11:23:04 +0100

    Added offer controllers

--- core/routes/store.route.js
+++ core/routes/store.route.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const verifyjwt = require("../../lib/middleware/verifyjwt");
 const isAdmin = require("../../lib/middleware/isAdmin");
 const hasStoreAccess = require("../../lib/middleware/hasStoreAccess");
+const hasOrderAccess = require("../../lib/middleware/hasOrderAccess");
 const storeController = require("../../lib/controllers/store.controller");
 const menuController = require("../../lib/controllers/menu.controller");
 const categoryController = require("../../lib/controllers/category.controller");
 const orderController = require("../../lib/controllers/order.controller");
+const offerController = require("../../lib/controllers/offer.controller");
 
 // GET METHODS
 router.get("/", storeController.getAllStores); // All Stores details
@@ -21,7 +23,7 @@ router.get(
   hasStoreAccess,
   orderController.getOrders
 ); // Store orders
-router.get("/:storeId/offers", storeController.getStoreOffers); // Store offer
+router.get("/:storeId/offers", offerController.getOffers); // Store offer
 router.get("/:storeId/feedback", storeController.getStoreFeedback); // Store feedback
 
 // POST METHODS
@@ -40,6 +42,7 @@ router.post(
 );
 
 router.post("/:storeId/orders", orderController.addOrder); // Anyone can order
+router.post("/:storeId/offers", verifyjwt, isAdmin, offerController.addOffer);
 
 // PATCH METHODS
 router.patch(
@@ -62,11 +65,25 @@ router.patch(
   hasStoreAccess,
   categoryController.updateCategory
 );
+
 router.patch(
   "/:storeId/orders/:orderId",
   verifyjwt,
-  hasStoreAccess,
+  (req, res, next) => {
+    hasStoreAccess(req, res, (err) => {
+      if (!err) return next(); // If hasStoreAccess passes, continue
+
+      hasOrderAccess(req, res, next); // Otherwise, check hasOrderAccess
+    });
+  },
   storeController.updateStore
+);
+
+router.patch(
+  "/:storeId/stores",
+  verifyjwt,
+  isAdmin,
+  offerController.updateOffer
 );
 
 // DELETE METHODS
@@ -95,20 +112,40 @@ router.delete(
   "/:storeId/categories",
   verifyjwt,
   isAdmin,
-  categoryController.deleteAllCategory
+  categoryController.deleteAllCategories
 );
 
 router.delete(
   "/:storeId/orders/:orderId",
   verifyjwt,
-  isAdmin,
+  (req, res, next) => {
+    isAdmin(req, res, (err) => {
+      if (!err) return next(); // If isAdmin passes, continue
+
+      hasOrderAccess(req, res, next); // Otherwise, check hasOrderAccess
+    });
+  },
   orderController.deleteOrder
 );
+
 router.delete(
   "/:storeId/orders",
   verifyjwt,
   isAdmin,
   orderController.deleteAllOrders
+);
+
+router.delete(
+  "/:storeId/offers/:offerId",
+  verifyjwt,
+  isAdmin,
+  offerController.deleteOffer
+);
+router.delete(
+  "/:storeId/offers",
+  verifyjwt,
+  isAdmin,
+  offerController.deleteAllOffers
 );
 
 module.exports = router;
